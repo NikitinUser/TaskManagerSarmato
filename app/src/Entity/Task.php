39146f52be57d1f8a72648b4939904c21760fb26
Vladimir Nikitin <jo.kent@mail.ru>
2023-09-14 20:26:03 +0400

    add entities, repositories

---
+++ app/src/Entity/Task.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 255)]
+    private string $title;
+
+    #[ORM\Column(length: 5000)]
+    private string $description;
+
+    #[ORM\Column]
+    private int $created_at;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $complitedDate = null;
+
+    #[ORM\Column]
+    private int $status = 0;
+
+    #[ORM\Column]
+    private int $userId;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): int
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(int $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getComplitedDate(): ?int
+    {
+        return $this->complitedDate;
+    }
+
+    public function setComplitedDate(?int $complitedDate): static
+    {
+        $this->complitedDate = $complitedDate;
+
+        return $this;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): static
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+}
