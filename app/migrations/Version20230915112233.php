4f1f66f69aa6a69168fd2289743277a3cbff58b5
Vladimir Nikitin <jo.kent@mail.ru>
2023-09-15 15:38:55 +0400

    add migration file

---
+++ app/migrations/Version20230915112233.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230915112233 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE IF NOT EXISTS user
+            (
+                id INT AUTO_INCREMENT NOT NULL,
+                login VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                PRIMARY KEY(id),
+                UNIQUE KEY IDX_LOGIN (login)
+            )
+            DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+        );
+
+        $this->addSql('CREATE TABLE IF NOT EXISTS task
+            (
+                id INT AUTO_INCREMENT NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                description VARCHAR(5000) NOT NULL,
+                created_at INT NOT NULL,
+                complited_date INT DEFAULT NULL,
+                status INT NOT NULL,
+                user_id INT NOT NULL,
+                PRIMARY KEY(id),
+                FOREIGN KEY (user_id)  REFERENCES user (id) ON DELETE CASCADE
+            )
+            DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+        );
+        
+        $this->addSql('CREATE TABLE IF NOT EXISTS messenger_messages
+            (
+                id BIGINT AUTO_INCREMENT NOT NULL,
+                body LONGTEXT NOT NULL,
+                headers LONGTEXT NOT NULL,
+                queue_name VARCHAR(190) NOT NULL,
+                created_at DATETIME NOT NULL,
+                available_at DATETIME NOT NULL,
+                delivered_at DATETIME DEFAULT NULL,
+                INDEX IDX_75EA56E0FB7336F0 (queue_name),
+                INDEX IDX_75EA56E0E3BD61CE (available_at),
+                INDEX IDX_75EA56E016BA31DB (delivered_at),
+                PRIMARY KEY(id)
+            )
+            DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE IF EXISTS task');
+        $this->addSql('DROP TABLE IF EXISTS user');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
