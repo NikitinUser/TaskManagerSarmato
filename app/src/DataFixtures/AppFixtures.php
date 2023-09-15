1f709ef3534ae1c3101144e113e95b4b5ca77f5e
Vladimir Nikitin <jo.kent@mail.ru>
2023-09-15 20:01:12 +0400

    add user fixture

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -4,14 +4,25 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // если записи с таким логином еще нет
-        // $user = new User();
-        // $manager->persist($product);
-        // $manager->flush();
+        $repository = $manager->getRepository(User::class);
+
+        $login = 'admin';
+        $password = '$2y$13$aGpyKVf27OBKRym4baLg2OCsrtBV8KM2M7jkTubk5XFhOAV6yN6Cm'; // 12345
+
+        $user = $repository->findOneByLogin($login);
+
+        if (empty($user)) {
+            $user = new User();
+            $user->setLogin($login)
+                ->setPassword($password);
+
+            $repository->create($user);
+        }
     }
 }
